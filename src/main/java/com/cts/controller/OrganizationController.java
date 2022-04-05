73c55141573d1c6b77a50da7bc4c38d0b5f894f4
krishna.ramachandran1973 <krishna.ramachandran1973@gmail.com>
2022-04-05 22:11:20 +0530

    Added security code

--- src/main/java/com/cts/controller/OrganizationController.java
+++ src/main/java/com/cts/controller/OrganizationController.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.security.RolesAllowed;
+
 @RequiredArgsConstructor
 @RestController
 @RequestMapping(value = "v1/organization")
@@ -15,21 +17,25 @@ public class OrganizationController {
     private final OrganizationService service;
 
 
+    @RolesAllowed({"ADMIN", "USER"})
     @GetMapping("/{organizationId}")
     public ResponseEntity<Organization> getOrganization(@PathVariable("organizationId") Long organizationId) {
         return ResponseEntity.ok(service.findById(organizationId));
     }
 
+    @RolesAllowed({"ADMIN", "USER"})
     @PutMapping("/{organizationId}")
     public void updateOrganization(@PathVariable("organizationId") Long id, @RequestBody Organization organization) {
         service.update(organization);
     }
 
+    @RolesAllowed({"ADMIN", "USER"})
     @PostMapping
     public ResponseEntity<Organization> saveOrganization(@RequestBody Organization organization) {
         return ResponseEntity.ok(service.create(organization));
     }
 
+    @RolesAllowed({"ADMIN"})
     @DeleteMapping("/{organizationId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteOrganization(@PathVariable("organizationId") Long organizationId, @RequestBody Organization organization) {
